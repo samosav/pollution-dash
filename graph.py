83114297884c48edf0ac75ae51e0b3f4d3d1f2b2
samosav <samosathegrandma@gmail.com>
2022-01-16 17:46:31 +0000

    first graphing

---
+++ graph.py
@@ -0,0 +1,27 @@
+import dash
+from dash import html
+import dash_core_components as dcc
+import plotly.graph_objects as go
+import pandas as pd
+
+df = pd.read_csv('test.csv', header=None)
+print(df)
+
+app = dash.Dash(__name__)
+fig = go.Figure()
+fig.add_scatter(x=df[0].to_list(), y=df[1].to_list())
+fig.show()
+fig.write_html('html_plot.html',
+               config={'toImageButtonOptions':
+                       {'format': 'svg'}})
+
+app.layout = html.Div([
+
+    dcc.Graph(
+        figure=fig
+
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
